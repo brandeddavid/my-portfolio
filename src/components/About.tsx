7fbede65ba0b8b8c252acf9dd8379d2d2468c4f8
David Mwangi <david.mathenge98@gmail.com>
2023-05-04 11:39:06 +0300

    build the about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,14 +1,41 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 type Props = {};
 
 const About = (props: Props) => {
 	return (
-		<div className="h-screen flex flex-col relative text-center md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
+		<motion.div
+			initial={{ opacity: 0 }}
+			transition={{ duration: 1.5 }}
+			whileInView={{ opacity: 1 }}
+			className="h-screen flex flex-col relative text-center md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center"
+		>
 			<h3 className="absolute top-24 uppercase tracking-[20px] text-2xl">
 				About
 			</h3>
-		</div>
+
+			<motion.img
+				src="https://brandeddavid.s3.eu-west-1.amazonaws.com/public/programmer.svg"
+				initial={{ x: -200, opacity: 0 }}
+				transition={{ duration: 1 }}
+				whileInView={{ opacity: 1, x: 0 }} // Only animates when the image is in view
+				viewport={{ once: true }} // Animate only on initial view
+				className="-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]"
+			/>
+			<div className="space-y-10 px-0 md:px-10">
+				<h4 className="text-4xl font-semibold">A little about me</h4>
+				<p className="text-base">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+					eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+					minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+					aliquip ex ea commodo consequat. Duis aute irure dolor in
+					reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+					pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+					culpa qui officia deserunt mollit anim id est laborum.
+				</p>
+			</div>
+		</motion.div>
 	);
 };
 
